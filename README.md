b49c9b41f3c2ec657b9003a417804bc3cdcd20f0
Kaifi Asif <kaifi.asif@blumeglobal.com>
2020-12-19 23:56:02 +0530

    Snake Game Final Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Snake-Game
+
+![Snake Game ](https://i.ibb.co/ss4q6Sc/Screenshot-13.jpg)
+
+## Setup:
+
+- run `npm i && npm start`
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Game from './components/Game';
+function App() {
+  return (
+    <div className='App'>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
